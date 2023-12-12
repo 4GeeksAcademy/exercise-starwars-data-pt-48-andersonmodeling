81231749f99d3cf3ab452d60caccddcf18380d84
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2023-12-12 11:32:44 +0000

    creando modelos

--- src/models.py
+++ src/models.py
@@ -27,6 +27,47 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(20), nullable=False)
+
+    
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    diameter = Column(String(50))
+    rotation = Column(String(50))
+    gravity = Column(String(50))
+    climate = Column(String(100))
+    terrain = Column(String(100))
+    orbital_period = Column(String(100))
+
+    
+    
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    height = Column(String(50))
+    mass = Column(String(50))
+    hair_color = Column(String(50))
+    eye_color = Column(String(50))
+    identification = Column(String(100))
+
+    
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    
+
+   
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
